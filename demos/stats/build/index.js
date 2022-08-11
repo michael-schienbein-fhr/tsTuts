c257f2ff2e0bc1978360a5d03577f28a64410649
Michael Schienbein <m@schienbein.dev>
2022-08-11 16:01:45 +0700

    update stats demo

--- demos/stats/build/index.js
+++ demos/stats/build/index.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log("hello world");
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+// import fs from 'fs';
+//
+// const matches = fs.readFileSync('football.csv', {
+//     encoding: 'utf-8'
+// })
+//     .split('\n')
+//     .map((row: string): string[] => {
+//         return row.split(',');
+//     });
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+console.log(reader.data[0]);
+console.log(dateOfFirstMatch);
+// enum MatchResult {
+//     HomeWin = "H",
+//     AwayWin = "A",
+//     Draw = "D"
+// }
+// const printMatchResult = (): MatchResult => {
+//     if (match[5] === 'H') {
+//         return MatchResult.HomeWin;
+//     }
+//     return MatchResult.AwayWin;
+// }
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- demos/stats/src/index.ts
+++ demos/stats/src/index.ts
@@ -1,1 +1,43 @@
-console.log("hello world");
+import {CsvFileReader} from "./CsvFileReader";
+import {MatchResult} from "./MatchResult";
+import {dateStringToDate} from "./utils";
+
+// import fs from 'fs';
+//
+// const matches = fs.readFileSync('football.csv', {
+//     encoding: 'utf-8'
+// })
+//     .split('\n')
+//     .map((row: string): string[] => {
+//         return row.split(',');
+//     });
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+console.log(reader.data[0]);
+console.log(dateOfFirstMatch);
+// enum MatchResult {
+//     HomeWin = "H",
+//     AwayWin = "A",
+//     Draw = "D"
+// }
+
+// const printMatchResult = (): MatchResult => {
+//     if (match[5] === 'H') {
+//         return MatchResult.HomeWin;
+//     }
+//     return MatchResult.AwayWin;
+// }
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
